0a323de93c7abb49362fcf345d75e3d473ced923
fitchdayo <taichee09@gmail.com>
2025-01-23 22:07:40 +0800

    Update button.js

--- button.js
+++ button.js
@@ -2,7 +2,16 @@
 var textList = [
   "are you really s?",
   "eh? hontou? hontou hontou??",
-  "ah I see.. ok can u press next plweasee :x"
+  "ah I see.. ok can u press next plweasee :x",
+  "firstly i'm sorry for this :x",
+  "but i would like to say, i'm very thankful to have met you",
+  "you have helped me alot during my dark times with just your va works.. :3",
+  "thank you for always making everyone feels safer with your work",
+  "lastly.."
+  "I would like to tell you that.."
+  "I like you."
+  "would you be okay if I were to ask you out? :3",
+  "you don't have to give me an answer now."
 ];
 
 var currentIndex = 0;
